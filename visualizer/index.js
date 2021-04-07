2d0ef6709229f342e418c3c38dd64f8e1f199e84
Jonathan Cui <jonathancui123@gmail.com>
2021-04-07 01:31:29 +0000

    feat: showing default graph in visualizer

--- visualizer/index.js
+++ visualizer/index.js
@@ -4,15 +4,24 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Enter a game ID into the URL path to view the breakdown");
+})
+
 app.get("/:gameId", (request, response) => {
   const { gameId } = request.params;
-
-  if (!gameId) {
-    response.send("Enter a game ID into the URL to view the breakdown");
-  } else {
-    response.sendFile(path.join(__dirname, "graph.html"));
-  }
+  response.sendFile(path.join(__dirname, "graph.html"));
 });
+
+// Serve the json data for the specified game
+app.get("/data/:gameId", (request, response) => {
+  try {
+    const {gameId} = request.params;
+  response.sendFile(path.join(__dirname, logs, `${gameId}.json`));
+  } catch (e) {
+    response.send(e);
+  }
+})
 
 app.listen(PORT, () =>
   console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`)

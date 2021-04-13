dde34961a49ed87636ebd07f57cd2caadfe74a20
Jonathan Cui <jonathancui123@gmail.com>
2021-04-12 20:19:50 -0400

    fix: code format

--- utils/minimax.js
+++ utils/minimax.js
@@ -3,24 +3,25 @@ const {
   getAdjacentCoordinate,
   coordinateOutOfBounds,
   coordinatesAreEqual,
-	boardToGrid,
-	findClosestApple,
-	distanceToClosestCorner
+  boardToGrid,
+  findClosestApple,
+  distanceToClosestCorner,
 } = require("./utils");
 
-const {
-  floodfill
-} = require("./floodfill")
+const { floodfill } = require("./floodfill");
 
 const { astar, Graph } = require("./pathfinding");
 
-const {MINIMAX_DEPTH, MAX_HEALTH, HEURISTIC_FUTURE_UNCERTAINTY_FACTOR} = require("../constants");
+const {
+  MINIMAX_DEPTH,
+  MAX_HEALTH,
+  HEURISTIC_FUTURE_UNCERTAINTY_FACTOR,
+} = require("../constants");
 
 // TODO: Fix "off by one error"
 // Game object which represents the board: (explanation: https://www.w3schools.com/js/js_object_constructors.asp)
 // Supports methods for modifying the board, undoing modifications, and getting potential moves
 function MinimaxGame(board) {
-
   // The state of the board at the current node of Minimax simulation
   // Battlesnake API board object
   this.board = board;
@@ -44,8 +45,6 @@ function MinimaxGame(board) {
     );
     const snakeTailCoordinate = currentSnake.body[currentSnake.body.length - 1];
 
-    
-
     // Create an object that describes the changes to the board on this move
     const newChange = {
       snake: {
@@ -58,14 +57,18 @@ function MinimaxGame(board) {
 
     // Grid Changes for floodFill
     // TODO: undo function for grid, account for food changes
-    
-    if(newSnakeHeadCoordinate.x >= 0 && newSnakeHeadCoordinate.x <= 10 &&
-    newSnakeHeadCoordinate.y >= 0 && newSnakeHeadCoordinate.y <= 10 ){
+
+    if (
+      newSnakeHeadCoordinate.x >= 0 &&
+      newSnakeHeadCoordinate.x <= 10 &&
+      newSnakeHeadCoordinate.y >= 0 &&
+      newSnakeHeadCoordinate.y <= 10
+    ) {
       this.grid[newSnakeHeadCoordinate.x][newSnakeHeadCoordinate.y] = 0;
       this.grid[snakeTailCoordinate.x][snakeTailCoordinate.y] = 1;
     }
- 
-		this.changeHistory.push(newChange);
+
+    this.changeHistory.push(newChange);
     // 'currentSnake' object is referencing an object in "this.board"
     // Modify the board object to reflect the changes
     currentSnake.head = newSnakeHeadCoordinate;
@@ -88,20 +91,26 @@ function MinimaxGame(board) {
       );
 
       // Adjustment for grid floodfill
-      if(lastChange.snake.newHeadPosition.x >= 0 && lastChange.snake.newHeadPosition.x <= 10 &&
-      lastChange.snake.newHeadPosition.y >= 0 && lastChange.snake.newHeadPosition.y <= 10 ){
-        this.grid[lastChange.snake.newHeadPosition.x][lastChange.snake.newHeadPosition.y] = 1
-        this.grid[lastChange.snake.prevTailPosition.x][lastChange.snake.prevTailPosition.y] = 0
+      if (
+        lastChange.snake.newHeadPosition.x >= 0 &&
+        lastChange.snake.newHeadPosition.x <= 10 &&
+        lastChange.snake.newHeadPosition.y >= 0 &&
+        lastChange.snake.newHeadPosition.y <= 10
+      ) {
+        this.grid[lastChange.snake.newHeadPosition.x][
+          lastChange.snake.newHeadPosition.y
+        ] = 1;
+        this.grid[lastChange.snake.prevTailPosition.x][
+          lastChange.snake.prevTailPosition.y
+        ] = 0;
       }
 
       // Opposite order of modifications made in move() method
       currentSnake.body.push(lastChange.snake.prevTailPosition);
       currentSnake.body.shift(); // .shift() gets rid of the first element of an array
       currentSnake.head = { ...currentSnake.body[0] }; // copy of head (don't reference)
-      
     }
   };
-
 }
 
 // depth: number of moves we want to continue looking forward
@@ -120,30 +129,42 @@ const calcBestMove = function (
 ) {
   // Base case: evaluate board at maximum depth
   if (remainingDepth === 0) {
-    value = evaluateBoard(game.board, mySnakeID, otherSnakeID, game.grid, remainingDepth);
-    if (logger){
-      logger.logCurrentNode({move: null, value})
+    value = evaluateBoard(
+      game.board,
+      mySnakeID,
+      otherSnakeID,
+      game.grid,
+      remainingDepth
+    );
+    if (logger) {
+      logger.logCurrentMoveAndValue({ move: null, value });
       logger.goToParent();
     }
     return [value, null];
   }
-  
+
   // Base case 2: evaluate board when either snake is dead
   // Note: should only evalute when it is our turn (takes 2 moves for a turn), so only
   // when maximizing player is true
   if (isMaximizingPlayer) {
-
-
-    const gameOverValue = evaluateIfGameOver(game.board, mySnakeID, otherSnakeID, remainingDepth);
+    const gameOverValue = evaluateIfGameOver(
+      game.board,
+      mySnakeID,
+      otherSnakeID,
+      remainingDepth
+    );
     if (gameOverValue) {
-      if (logger){
-        logger.logCurrentNode({move: null, value: gameOverValue, gameOver: true})
+      if (logger) {
+        logger.logCurrentMoveAndValue({
+          move: null,
+          value: gameOverValue,
+          gameOver: true,
+        });
         logger.goToParent();
       }
       return [gameOverValue, null];
     }
   }
-  
 
   // clean up dead snakes and eaten food before proceeding with simulation
   game.cleanupBoard();
@@ -170,8 +191,8 @@ const calcBestMove = function (
     var move = possibleMoves[i];
     // Make the move, but undo before exiting loop
     game.move(move, targetSnakeID);
-    
-    if (logger){
+
+    if (logger) {
       logger.goDeeper(move);
     }
     // Recursively get the value from this move
@@ -183,7 +204,7 @@ const calcBestMove = function (
       logger,
       alpha,
       beta,
-      !isMaximizingPlayer,
+      !isMaximizingPlayer
     )[0];
     // Log the value of this move
     // console.log(isMaximizingPlayer ? 'Max: ' : 'Min: ', depth, move, value, bestMove, bestMoveValue);
@@ -208,15 +229,15 @@ const calcBestMove = function (
     // Check for alpha beta pruning
     if (beta <= alpha) {
       // console.log('Prune', alpha, beta);
-      if (logger){
+      if (logger) {
         logger.pruningAtCurrentNode();
       }
       break;
     }
   }
 
-  if (logger){
-    logger.logCurrentNode({move: bestMove, value: bestMoveValue})
+  if (logger) {
+    logger.logCurrentMoveAndValue({ move: bestMove, value: bestMoveValue });
     logger.goToParent();
   }
 
@@ -231,14 +252,14 @@ const evaluateIfGameOver = (board, mySnakeID, otherSnakeID, remainingDepth) => {
   const otherSnakeHead = otherSnake.head;
   let headOnCollision = false;
   let mySnakeDead = false;
-	let mySnakeMaybeDead = false; // opponent may not use the best move (head collisions)
+  let mySnakeMaybeDead = false; // opponent may not use the best move (head collisions)
   let otherSnakeDead = false;
   let otherSnakeMaybeDead = false;
 
   // my snake head or other snake head out of bounds
-	// being out of bounds is the only death that is certain
-  
-  if(coordinateOutOfBounds(mySnakeHead, board.height, board.width)){
+  // being out of bounds is the only death that is certain
+
+  if (coordinateOutOfBounds(mySnakeHead, board.height, board.width)) {
     mySnakeDead = true;
   }
 
@@ -283,20 +304,20 @@ const evaluateIfGameOver = (board, mySnakeID, otherSnakeID, remainingDepth) => {
     }
     if (mySnakeHeadCollisions > 1) {
       // remember to do this
-      console.log(mySnakeHeadCollisions)
+      console.log(mySnakeHeadCollisions);
       otherSnakeDead = true;
     }
   }
-  
+
   const adjustForFutureUncertainty = (score) => {
     // return score* (HEURISTIC_FUTURE_UNCERTAINTY_FACTOR**(MINIMAX_DEPTH - remainingDepth))
     return score;
-  }
+  };
 
   if (mySnakeDead) {
     return adjustForFutureUncertainty(-1000);
-	} else if (mySnakeMaybeDead) {
-		return adjustForFutureUncertainty(-500);
+  } else if (mySnakeMaybeDead) {
+    return adjustForFutureUncertainty(-500);
   } else if (otherSnakeMaybeDead) {
     return adjustForFutureUncertainty(500);
   } else if (otherSnakeDead) {
@@ -307,77 +328,105 @@ const evaluateIfGameOver = (board, mySnakeID, otherSnakeID, remainingDepth) => {
 };
 
 // Scores the given game board --> higher score if good for mySnake, lower if bad for mySnake
-const evaluateBoard = (board, mySnakeID, otherSnakeID, grid, remainingDepth) => {
-	// range = [-1000, 1000]
-	// score will only be negative if it might die
-	var score = 0;
+const evaluateBoard = (
+  board,
+  mySnakeID,
+  otherSnakeID,
+  grid,
+  remainingDepth
+) => {
+  // range = [-1000, 1000]
+  // score will only be negative if it might die
+  var score = 0;
 
-	// gameOverValue is -500 if mySnake might die
-	// gameOverValue is -1000 if mySnake will for sure die
-  const gameOverValue = evaluateIfGameOver(board, mySnakeID, otherSnakeID, remainingDepth);
-	if (gameOverValue) {
-  	score += gameOverValue;
-	}
-	
-	const mySnake = board.snakes.find((snake) => snake.id === mySnakeID);
+  // gameOverValue is -500 if mySnake might die
+  // gameOverValue is -1000 if mySnake will for sure die
+  const gameOverValue = evaluateIfGameOver(
+    board,
+    mySnakeID,
+    otherSnakeID,
+    remainingDepth
+  );
+  if (gameOverValue) {
+    score += gameOverValue;
+  }
+
+  const mySnake = board.snakes.find((snake) => snake.id === mySnakeID);
   const otherSnake = board.snakes.find((snake) => snake.id === otherSnakeID);
-	const mySnakeHead = mySnake.head;
-	const otherSnakeHead = otherSnake.head;
+  const mySnakeHead = mySnake.head;
+  const otherSnakeHead = otherSnake.head;
   const MAX_DISTANCE = board.width + board.height;
 
-	// if snake is hungry, the closer the snake to food the better
+  // if snake is hungry, the closer the snake to food the better
   const mySnakeLength = mySnake.length;
   const closestApple = findClosestApple(board.food, mySnakeHead);
-	if (mySnake.health <= 40 || (mySnakeLength < otherSnake.length + 2 && mySnakeLength < 15)) {
-		const closestAppleDistance = Math.abs(mySnakeHead.x - closestApple.x) + Math.abs(mySnakeHead.y - closestApple.y);
-    
-    const foodScore = ((MAX_DISTANCE - closestAppleDistance)/4)**2 + ((MAX_HEALTH - mySnake.health)/5)**2
+  if (
+    mySnake.health <= 40 ||
+    (mySnakeLength < otherSnake.length + 2 && mySnakeLength < 15)
+  ) {
+    const closestAppleDistance =
+      Math.abs(mySnakeHead.x - closestApple.x) +
+      Math.abs(mySnakeHead.y - closestApple.y);
+
+    const foodScore =
+      ((MAX_DISTANCE - closestAppleDistance) / 4) ** 2 +
+      ((MAX_HEALTH - mySnake.health) / 5) ** 2;
     console.log(foodScore);
     score += foodScore; // heuristic score can be adjusted later
-	}
+  }
 
   let cavernSize;
 
   // floodfill heuristic
-  if(!coordinateOutOfBounds(mySnakeHead,board.height, board.width)){
+  if (!coordinateOutOfBounds(mySnakeHead, board.height, board.width)) {
     grid[mySnakeHead.x][mySnakeHead.y] = 1;
-    cavernSize = floodfill(grid, mySnakeHead.x, mySnakeHead.y, mySnakeLength * 2);
-    console.log("cavernSize for "+ mySnakeHead.x +", "+ mySnakeHead.y + ":" +cavernSize);
+    cavernSize = floodfill(
+      grid,
+      mySnakeHead.x,
+      mySnakeHead.y,
+      mySnakeLength * 2
+    );
+    console.log(
+      "cavernSize for " +
+        mySnakeHead.x +
+        ", " +
+        mySnakeHead.y +
+        ":" +
+        cavernSize
+    );
     grid[mySnakeHead.x][mySnakeHead.y] = 0;
   }
-
 
   if (cavernSize <= mySnakeLength) {
     score = -1000;
     return score;
   }
 
-	// the further from the edge the better
-	// add the minimum distance between the x and y distance
-	const distanceFromEdge = 0;
-	var x = 0;
-	var y = 0;
+  // the further from the edge the better
+  // add the minimum distance between the x and y distance
+  const distanceFromEdge = 0;
+  var x = 0;
+  var y = 0;
 
-	if (mySnakeHead.x < board.width / 2) {
-		x = mySnakeHead.x;
-	} else {
-		x = board.width - mySnakeHead.x;
-	}
+  if (mySnakeHead.x < board.width / 2) {
+    x = mySnakeHead.x;
+  } else {
+    x = board.width - mySnakeHead.x;
+  }
 
-	if (mySnakeHead.y < board.width / 2) {
-		y = mySnakeHead.y;
-	} else {
-		y = board.width - mySnakeHead.y;
-	}
+  if (mySnakeHead.y < board.width / 2) {
+    y = mySnakeHead.y;
+  } else {
+    y = board.width - mySnakeHead.y;
+  }
 
-	score += Math.min(x, y) * 10 + 10;
+  score += Math.min(x, y) * 10 + 10;
 
+  // check distance of enemy head from corner
+  score += MAX_DISTANCE - distanceToClosestCorner(otherSnakeHead, board);
 
-	// check distance of enemy head from corner
-	score += MAX_DISTANCE - distanceToClosestCorner(otherSnakeHead, board);
-
-	return score;
-  // When the enemy snake is close to the edge, attempt to get closer to 
+  return score;
+  // When the enemy snake is close to the edge, attempt to get closer to
   // their head (blocks their path). Keep in mind to avoid moves that may lead to death.
 };
 
--- visualizer/minimaxLogger.js
+++ visualizer/minimaxLogger.js
@@ -1,112 +1,129 @@
 // Used by the minimax algorithm to output game logs
-const {writeFileSync, statSync, readFileSync, existsSync, mkdirSync} = require('fs');
+const {
+  writeFileSync,
+  statSync,
+  readFileSync,
+  existsSync,
+  mkdirSync,
+} = require("fs");
 const path = require("path");
 
-const {LOGGER_TURNS_TO_KEEP_BEFORE_OVERWRITE} = require("../constants");
+const { LOGGER_TURNS_TO_KEEP_BEFORE_OVERWRITE } = require("../constants");
 
 // Stores the data necessary for the visualizer to represent this node in a graph
-function VisualizerNode(minimaxLoggerNode, loggerTurnNumber){
-  if (minimaxLoggerNode.parentId){
+function VisualizerNode(minimaxLoggerNode, loggerTurnNumber) {
+  const newTurnNumber =
+    loggerTurnNumber + Math.floor(minimaxLoggerNode.nodeDepth / 2);
+  const isMaximizingPlayer = !Boolean(minimaxLoggerNode.nodeDepth % 2); // our turn will be on even node depth
+  const our_their = isMaximizingPlayer ? "OUR" : "THEIR";
+  const their_our = isMaximizingPlayer ? "THEIR" : "OUR";
+  const whose_move = minimaxLoggerNode.data.move
+    ? isMaximizingPlayer
+      ? "our move"
+      : "their move"
+    : "no move";
+
+  if (minimaxLoggerNode.parentId) {
     this.parent = minimaxLoggerNode.parentId;
   }
-  this.key = minimaxLoggerNode.nodeId
-
-  const newTurnNumber = loggerTurnNumber + Math.floor(minimaxLoggerNode.nodeDepth / 2);
-  const isMaximizingPlayer = !Boolean(minimaxLoggerNode.nodeDepth % 2) // our turn will be on even node depth
-  const our_their = isMaximizingPlayer ? "OUR" : "THEIR";
-  const their_our = isMaximizingPlayer ?  "THEIR" : "OUR";
-  const whose_move = minimaxLoggerNode.data.move ? (isMaximizingPlayer ? "our move" : "their move"): "no move"
+  this.key = minimaxLoggerNode.nodeId;
   this.name = `Turn ${newTurnNumber}, ${whose_move}`;
 
-  // Colours var for the 
+  // Colours var for the block
   var colors = {
-  'red': '#be4b15',
-  'green': '#52ce60',
-  'blue': '#6ea5f8',
-  'lightred': '#fd8852',
-  'lightblue': '#afd4fe',
-  'lightgreen': '#b9e986',
-  'pink': '#faadc1',
-  'purple': '#d689ff',
-  'orange': '#fdb400',
-  }   
+    red: "#be4b15",
+    green: "#52ce60",
+    blue: "#6ea5f8",
+    lightred: "#fd8852",
+    lightblue: "#afd4fe",
+    lightgreen: "#b9e986",
+    pink: "#faadc1",
+    purple: "#d689ff",
+    orange: "#fdb400",
+  };
 
+  // list items design
   this.items = [];
-  if(minimaxLoggerNode.previousMove){
+  if (minimaxLoggerNode.previousMove) {
     this.items.push({
       item: `If ${their_our} snake moved ${minimaxLoggerNode.previousMove.toUpperCase()}:`,
       iskey: true,
       figure: "",
       fill: "",
-      stroke: ""
-  })
+      stroke: "",
+    });
   }
-  if (minimaxLoggerNode.data.move){
+  if (minimaxLoggerNode.data.move) {
     // Not a leaf node
     this.items.push({
       item: `${our_their} snake will move ${minimaxLoggerNode.data.move.toUpperCase()}:`,
       iskey: false,
       figure: "",
       fill: "",
-      stroke: ""
-    })
-  } else if(minimaxLoggerNode.data.gameOver) {
+      stroke: "",
+    });
+  } else if (minimaxLoggerNode.data.gameOver) {
     // Game over leaf node
     this.items.push({
       item: `The game is over with`,
       iskey: false,
       figure: "Circle",
       fill: colors.red,
-      stroke: colors.red
-    })
+      stroke: colors.red,
+    });
   } else {
     // Max depth leaf node
-        this.items.push({
+    this.items.push({
       item: `The game is evaluated at`,
       iskey: false,
       figure: "",
       fill: "",
-      stroke: ""
-    })
+      stroke: "",
+    });
   }
   this.items.push({
     item: `Value: ${minimaxLoggerNode.data.value}:`,
     iskey: false,
     figure: "",
     fill: "",
-    stroke: ""
-  })
+    stroke: "",
+  });
 }
 
 // Stores the data for a node (a game board state) in the minimax tree
-function MinimaxLoggerNode(depth, childNumber='0', parentId=undefined, previousMove=undefined){
+function MinimaxLoggerNode(
+  depth,
+  childNumber = "0",
+  parentId = undefined,
+  previousMove = undefined
+) {
   // To create a unique nodeId, we concatenate the childNumber onto the parentId string
-  if (parentId !== undefined){ // parentId is only undefined for the root node
+  if (parentId !== undefined) {
+    // parentId is only undefined for the root node
     this.parentId = String(parentId);
-    this.nodeId = String(this.parentId +`-${childNumber}`);
+    this.nodeId = String(this.parentId + `-${childNumber}`);
   } else {
-    this.nodeId = '0';
+    this.nodeId = "0";
   }
 
-  if (previousMove){
+  if (previousMove) {
     this.previousMove = previousMove;
   }
-  
+
   this.nodeDepth = depth;
 
   this.childCount = 0;
 
-  this.data = {}
+  this.data = {};
 }
 
 // Globally available object that implements navigation methods to track of the current position in the minimax simulation tree and log everything in the right place
-function MinimaxLogger(gameId, turnNumber){
+function MinimaxLogger(gameId, turnNumber) {
   // store game Id and turn number to write to correct log
   this.gameId = gameId;
   this.turnNumber = turnNumber;
-  
-  
-  this.currentNode = undefined
+
+  this.currentNode = undefined;
 
   // Use a stack to keep track of the DFS-like traversal of the minimax tree
   // Each element of the stack is of type MinimaxLoggerNode and represents a node (a game board state) in the minimax tree -- holds all nodes we are still processing (up to and including the currentNode)
@@ -114,90 +131,94 @@ function MinimaxLogger(gameId, turnNumber){
 
   // VisualizerNodes of the minimax simulation states that we have finished processing
   // A list to collect our log data before writing it to json
-  this.finishedNodes = []
+  this.finishedNodes = [];
 
-  this.init = function() {
+  this.init = function () {
     this.currentNode = new MinimaxLoggerNode(0);
     this.nodeStack.push(this.currentNode);
-  }
+  };
 
   // Step one level deeper in the simulation than before
-  this.goDeeper = function(previousMove) {
-
-    // create a new child node and set it as the new currentNode    
+  this.goDeeper = function (previousMove) {
+    // create a new child node and set it as the new currentNode
     const newNodeParentId = this.currentNode.nodeId;
     const newNodeChildNumber = String(this.currentNode.childCount);
 
     this.currentNode.childCount++;
-    this.currentNode = new MinimaxLoggerNode(this.nodeStack.length, newNodeChildNumber, newNodeParentId, previousMove);
+    this.currentNode = new MinimaxLoggerNode(
+      this.nodeStack.length,
+      newNodeChildNumber,
+      newNodeParentId,
+      previousMove
+    );
 
     // push new node to nodeStack
     this.nodeStack.push(this.currentNode);
 
-    return
-  }
+    return;
+  };
 
   // Step one level up in the simulation depth
-  this.goToParent = function() {
-
+  this.goToParent = function () {
     // pop the node off of the nodeStack (we are finished processing it)
     const newFinishedNode = this.nodeStack.pop();
 
-    if (this.nodeStack.length > 0) { // handle empty nodeStack for the last node we process (root node)
+    if (this.nodeStack.length > 0) {
+      // handle empty nodeStack for the last node we process (root node)
       this.currentNode = this.nodeStack[this.nodeStack.length - 1];
     }
 
     // We will never return to this node, commit it's contents to the finishedNodes
-    const newVisualizerNode = new VisualizerNode(newFinishedNode, turnNumber)
+    const newVisualizerNode = new VisualizerNode(newFinishedNode, turnNumber);
     this.finishedNodes.push(newVisualizerNode);
 
-    return
-  }
+    return;
+  };
 
   // Records that the current node gets pruned
-  this.pruningAtCurrentNode = function() {
-    return
-  }
+  this.pruningAtCurrentNode = function () {
+    return;
+  };
 
   // Records/Updates the logged data for the current simulation node
   // info: e.g. {move: "up/down/left/right", value: 1000} <-- value at this node
-  this.logCurrentNode = function(info) {
+  this.logCurrentMoveAndValue = function (info) {
     // should take in an object with optional properties
     Object.assign(this.currentNode.data, info);
-    return
-  } 
+    return;
+  };
 
   // Writes the logged data to a json file after a response has been given to Battlesnake game server
-  this.writeLogsToJson = function() {
+  this.writeLogsToJson = function () {
     const folderpath = path.join(__dirname, `logs`);
-    if (!existsSync(folderpath)){
+    if (!existsSync(folderpath)) {
       mkdirSync(folderpath);
     }
 
-    const filepath = path.join(__dirname, `logs`, `${this.gameId}.json`)
-    try { 
-      statSync(filepath) // Will throw if the file doesn't exist
+    const filepath = path.join(__dirname, `logs`, `${this.gameId}.json`);
+    try {
+      statSync(filepath); // Will throw if the file doesn't exist
       // some other turn, file already exists
-      if (this.turnNumber % LOGGER_TURNS_TO_KEEP_BEFORE_OVERWRITE == 0){
-        // Reset the log file for this game every LOGGER_TURNS_TO_KEEP turns 
+      if (this.turnNumber % LOGGER_TURNS_TO_KEEP_BEFORE_OVERWRITE == 0) {
+        // Reset the log file for this game every LOGGER_TURNS_TO_KEEP turns
         console.log(this.turnNumber);
-         throw "Logs full"
-      } 
+        throw "Logs full";
+      }
       const jsonFile = readFileSync(filepath);
       const jsonObject = JSON.parse(jsonFile);
       jsonObject[`turn_${this.turnNumber}`] = this.finishedNodes;
       const newJsonFile = JSON.stringify(jsonObject);
       writeFileSync(filepath, newJsonFile);
-      
-    } catch { // first turn of the game, file doesn't exist yet
-      const newJsonObject = {}
+    } catch {
+      // first turn of the game, file doesn't exist yet
+      const newJsonObject = {};
       newJsonObject[`turn_${this.turnNumber}`] = this.finishedNodes;
       const newJsonFile = JSON.stringify(newJsonObject);
       writeFileSync(filepath, newJsonFile);
     }
-  }
+  };
 }
 
 module.exports = {
-  MinimaxLogger
-}
+  MinimaxLogger,
+};

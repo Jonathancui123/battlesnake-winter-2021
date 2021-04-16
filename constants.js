9763381381bc6eace8b8582d74b851d83081cba1
Tianhui Yang <tianhui.yg@gmail.com>
2021-04-15 23:23:36 -0400

    chore: merge conflict #2

--- constants.js
+++ constants.js
@@ -1,12 +1,7 @@
 module.exports = {
   // Logger
-<<<<<<< HEAD
   USE_LOGGER : true,
   LOGGER_TURNS_TO_KEEP_BEFORE_OVERWRITE: 100,
-=======
-  USE_LOGGER : false,
-  LOGGER_TURNS_TO_KEEP_BEFORE_OVERWRITE: 30,
->>>>>>> 426ea949c41f20fd7e05c58f9a3844c5298fc0b9
   COLOURS : {
     red: "#be4b15",
     green: "#52ce60",
@@ -20,11 +15,7 @@ module.exports = {
   },
   // How many moves to simulate
   // Two moves (one from each snake) is one "turn" in the game. e.g. MINIMAX_DEPTH=2 means that we will only simulate the immediate turn
-<<<<<<< HEAD
-  MINIMAX_DEPTH : 4,
-=======
   MINIMAX_DEPTH : 8,
->>>>>>> 426ea949c41f20fd7e05c58f9a3844c5298fc0b9
   MAX_HEALTH: 100,
   // Heuristics:
   HEURISTIC_FUTURE_UNCERTAINTY_FACTOR: 0.87,
